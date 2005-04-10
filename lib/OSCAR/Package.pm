eeca65b7464c5498d2c3ba8ccbfaee3d79f0769e
gvallee <gvallee@3d29fa8e-40fc-0310-b3e8-bdbe099eca24>
2005-04-10 03:24:28 +0000

    Simplify the local reposity settings
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar-debian/trunk@78 3d29fa8e-40fc-0310-b3e8-bdbe099eca24

--- lib/OSCAR/Package.pm
+++ lib/OSCAR/Package.pm
@@ -56,7 +56,7 @@ if ($distro_name ne 'debian') {
     $PKG_SEPARATOR = '-';
 } else {
     # debian
-    $RPM_POOL = $ENV{OSCAR_RPMPOOL} || '/tftpboot/debian/testing/main/binary-i386';
+    $RPM_POOL = $ENV{OSCAR_RPMPOOL} || '/tftpboot/debian';
     $PKG_EXTENSION = ".deb";
     $PKG_DIRNAME = "Debs";
     $PKG_SEPARATOR = '_';
